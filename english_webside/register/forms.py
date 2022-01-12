24c3c2d20c71db7f9bae87f5d2d9316d16683393
Krzysztof Lach <lachkrzysztof56@gmail.com>
2022-01-12 19:30:29 +0100

    Improving the appearance of the registration and login page

--- english_webside/register/forms.py
+++ english_webside/register/forms.py
@@ -3,9 +3,16 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    age = forms.IntegerField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    age = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     
     class Meta:
         model = User
         fields = ('username', 'age', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
